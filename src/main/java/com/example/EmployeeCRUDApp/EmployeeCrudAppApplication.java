43e12a667b64bc9cb069161825624bb0545f3b7e
smkun <skunian@yahoo.com>
2024-06-17 17:16:46 -0400

    init commit

---
+++ src/main/java/com/example/EmployeeCRUDApp/EmployeeCrudAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.EmployeeCRUDApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeCrudAppApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/EmployeeCRUDApp/service/EmployeeService.java
@@ -0,0 +1,40 @@
+package com.example.EmployeeCRUDApp.service;
+
+import com.example.EmployeeCRUDApp.model.Employee;
+import com.example.EmployeeCRUDApp.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public void saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    public Employee getEmployeeById(Long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+        if (optional.isPresent()) {
+            employee = optional.get();
+        } else {
+            throw new RuntimeException("Employee not found for id :: " + id);
+        }
+        return employee;
+    }
+
+    public void deleteEmployeeById(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
+
